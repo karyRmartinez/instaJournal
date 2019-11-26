4a78758781a48c602f98366949a3841e05bb4988
karyRmartinez <karymartinez@pursuit.org>
2019-11-26 16:48:19 -0500

    starting to populate profile ViewController

--- Controllers/ProfileViewController.swift
+++ Controllers/ProfileViewController.swift
@@ -12,14 +12,43 @@ class ProfileViewController: UIViewController {
     
     var settingFromLogin = false
 
+    var image = UIImage() {
+         didSet {
+             self.profileImage.image = image
+         }
+     }
+    var imageURL: String? = nil
+    
+    
+    //MARK: UI OBJ
+    
     lazy var profileLabel: UILabel = {
         let label = UILabel()
         label.backgroundColor = .white
         label.textColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+          label.font = UIFont(name: "Optima-BOld", size: 25)
         label.text = "Profile"
         return label
     }()
-
+    
+  var profileImage: UIImageView = {
+      let image = UIImageView()
+      image.image = UIImage(named: "noImage")
+    image.image = #imageLiteral(resourceName: <#T##String#>)
+      image.contentMode = .scaleAspectFill
+      return image
+  }()
+    
+    
+    var addButton: UIButton = {
+         let button = UIButton()
+         button.setImage(UIImage(named: "add"), for: .normal)
+//         button.addTarget(self, action: #selector(presentPhotoPickerController), for: .touchUpInside)
+         return button
+     }()
+    
+    
+    
     private func setupHeaderLabel() {
            view.addSubview(profileLabel)
            
@@ -31,7 +60,15 @@ class ProfileViewController: UIViewController {
                profileLabel.heightAnchor.constraint(lessThanOrEqualTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.08)])
        }
        
-    
+    private func setImageConstraints() {
+         view.addSubview(profileImage)
+         profileImage.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             profileImage.centerXAnchor.constraint(equalTo: profileLabel.centerXAnchor),
+             profileImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+             profileImage.widthAnchor.constraint(equalToConstant: 200),
+             profileImage.heightAnchor.constraint(equalToConstant: 200)])
+     }
     
     
     
@@ -39,8 +76,17 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupHeaderLabel()
+        setImageConstraints()
 
     }
 
 
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+    }
 }
